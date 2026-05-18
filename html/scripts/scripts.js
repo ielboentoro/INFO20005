7681e2e9139f6c6c583f27231caa11c083a6aed8
ielboentoro <142801614+ielboentoro@users.noreply.github.com>
2026-05-18 21:44:16 +1000

    Cart is working with a modular design that allows for the script to be appended with more products on demand without additional commands.

--- html/scripts/scripts.js
+++ html/scripts/scripts.js
@@ -6,7 +6,7 @@ products_arr = [
 
         {title: "Chocho",
         price: "24.99",
-        img_src1: "images/sakura.png"}
+        img_src1: "images/chocho.png"}
     ]
 
 
@@ -14,12 +14,18 @@ products_arr = [
 const params = new URLSearchParams(window.location.search);
 const productData = params.get("name");
 
-var cart = [];
-
-console.log(productData);
+let cart = JSON.parse(localStorage.getItem('cart'))|| [];
 
 window.onload = function() {
-    initPage(productData);
+    const params = new URLSearchParams(window.location.search);
+    const data = params.get("name");
+
+    if(data === "Cart"){
+        initCart(cart);
+    }
+    else{
+        initPage(productData);
+    }
 }
 
 function initPage(product) {
@@ -31,14 +37,109 @@ function initPage(product) {
 function addToCart(product){
     const item = products_arr.find(element => element.title === product);
 
+    console.log(item);
+
+    const params = new URLSearchParams(window.location.search);
+    const data = params.get("name");
+
     if(cart.find(element => element.title === product)){
         cart.find(element => element.title === product).quantity++;
     }
     else{
-        cart.push({title: item.title, price: item.price, quantity: 1});
+        cart.push({title: item.title, price: item.price, img_src1: item.img_src1, quantity: 1});
     }
 
-    //debugging output
+    if(data === "Cart"){
+        refreshCart(cart);
+    }
 
-    console.log(cart);
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
+
+function deleteQtyCart(product){
+    const item = products_arr.find(element => element.title === product);
+
+    console.log(item);
+
+    const params = new URLSearchParams(window.location.search);
+    const data = params.get("name");
+
+    if(cart.find(element => element.title === product)){
+        if(cart.find(element => element.title === product).quantity !== 1){
+            cart.find(element => element.title === product).quantity--;
+        }
+        else{
+            cart.find(element => element.title === product).quantity--;
+            deleteFromCart(product);
+        }
+    }
+
+    if(data === "Cart"){
+        refreshCart(cart);
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function deleteFromCart(product){
+    const params = new URLSearchParams(window.location.search);
+    const data = params.get("name");
+
+    const toDelete = cart.find(element => element.title === product);
+
+    cart.find(element => element.title === product).quantity = 0;
+
+    cart.splice(cart.indexOf(toDelete), 1);
+
+    if(data === "Cart"){
+        refreshCart(cart);
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function initCart(cart){
+    for(i = 0; i < cart.length; i++){
+        updateCartHTML(cart[i]);
+    }
+}
+
+function clearCartVis(){
+    const elem = document.getElementById("cartContent");
+
+    elem.innerHTML = ``;
+}
+
+function refreshCart(cart){
+    clearCartVis();
+    initCart(cart);
+}
+
+function updateCartHTML(product){
+    const elem = document.getElementById("cartContent");
+
+    elem.innerHTML += 
+    `<div class="cartBox" id="${product.title}">
+        <div class="preview">
+            <image class="small-img" src="${product.img_src1}" width="auto"></image>
+        </div>
+
+        <div class="title">
+            <a class="section-header product-description">${product.title}</a>
+        </div>
+
+        <div class="aux">
+            <br>
+            <a class="product-description" id="${product.title}totalPrice">Total Price: ${product.price * product.quantity}</a>
+            <br>
+            <a>Quantity: ${product.quantity}</a>
+            <br>
+
+            <button onclick="deleteFromCart('${product.title}')">Delete</button>
+            <button onclick="addToCart('${product.title}')">Add Quantity</button>
+            <button onclick="deleteQtyCart('${product.title}')">Reduce Quantity</button>
+        </div>
+    </div>`
+}
+
+ 
