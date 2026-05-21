7fe50a7f863425649f82ff1ca06053916c5e5ed2
ielboentoro <142801614+ielboentoro@users.noreply.github.com>
2026-05-21 14:43:37 +1000

    Filtering system partially working. TODO: style cartBox and add searching.

--- html/scripts/scripts.js
+++ html/scripts/scripts.js
@@ -2,19 +2,22 @@
 products_arr = [
         {title: "Sakura",
         price: "24.99",
-        img_src1: "images/sakura.png"},
+        img_src1: "images/sakura.png",
+        href: "sakura.html",
+        tags: ['strong', 'espresso']},
 
         {title: "Chocho",
         price: "24.99",
-        img_src1: "images/chocho.png"}
+        img_src1: "images/chocho.png",
+        href: "chocho.html",
+        tags: ['mild', 'filter']}
     ]
-
-
 
 const params = new URLSearchParams(window.location.search);
 const productData = params.get("name");
 
-let cart = JSON.parse(localStorage.getItem('cart'))|| [];
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+let productFilters = []
 
 window.onload = function() {
     const params = new URLSearchParams(window.location.search);
@@ -23,8 +26,34 @@ window.onload = function() {
     if(data === "Cart"){
         initCart(cart);
     }
-    else{
+    else if(data){
         initPage(productData);
+    }
+}
+
+let checkBoxes;
+let filters;
+
+function initFilters(){
+    checkBoxes = document.querySelectorAll("input[type=checkbox]");
+    checkBoxes.forEach(element => element.addEventListener("change", filterCart));
+
+    filters = {
+        strengthStrong: ["strong"],
+        strengthMild: ["mild"],
+        strengthWeak: ["weak"]
+    }
+}
+
+function filterCart(){
+    document.getElementById("product-view").innerHTML = ``;
+
+    productFilters = Array.from(checkBoxes).filter(element => element.checked).flatMap(element => filters[element.getAttribute('name')]);
+
+    for(i = 0; i < products_arr.length; i++){
+        if(productFilters.every(value => products_arr[i].tags.includes(value))){
+            updateStorePage(products_arr[i]);
+        }
     }
 }
 
@@ -110,6 +139,12 @@ function initMiniCart(){
     }
 }
 
+function initProductPage(){
+    for(i = 0; i < products_arr.length; i++){
+        updateStorePage(products_arr[i]);
+    }
+}
+
 function clearCartVis(){
     const elem = document.getElementById("cartContent");
 
@@ -166,6 +201,35 @@ function updateMiniCartHTML(product){
             <a class="product-description" id="${product.title}totalPrice">Total Price: ${product.price * product.quantity}</a>
             <br>
             <a>Quantity: ${product.quantity}</a>
+        </div>
+    </div>`
+}
+
+function updateStorePage(product){
+    const elem = document.getElementById("product-view");
+
+    elem.innerHTML +=
+    `<div class="product">
+        <div class="image">
+            <image src="${product.img_src1}" width="270px"></image>
+        </div>
+        
+        <div class="product-text">
+            <a class="preview-text">
+                ${product.title}
+            </a>
+        </div>
+
+        <div class="price">
+            <a class="preview-text price">
+                ${product.price}
+            </a>
+        </div>
+
+        <div class="button">
+            <button class="product-button" onclick="window.location.href='${product.href}?name=${product.title}'">
+                View
+            </button>
         </div>
     </div>`
 }
